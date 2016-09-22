7abed4086ab627b5fe57d2a83ea64293d0b95aca
Michael <realq86@gmail.com>
2016-09-21 18:45:05 -0700

    Change the architecture of the code so: 1. math functions do not call any views, and return values. 2. View changing methods just accepts values to update views and return. 3. Control functions fetch values from views or NSUserdefault and calls math methods, and calls view methods with results of math methods.
    This way math functions can easily be tested changed, or extracted else where.
    And Control functions can be easily called in relevant viewController life cycle functions.

--- TipCalculator/TipViewController.swift
+++ TipCalculator/TipViewController.swift
@@ -18,47 +18,64 @@ class TipViewController: UIViewController {
     
     let tipAmountArray = [0.15, 0.20, 0.25]
     
+    var tipValue = 0.00
+    var totalValue = 0.00
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         self.loadDefualtSettings()
-        print("test")
+        print("viewDidLoad")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("viewWillAppear")
+        self.loadDefualtSettings()
+        self.updateAllFields()
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did appear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("view will disappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("view did disappear")
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    
     func loadDefualtSettings() {
         let userDefualts = UserDefaults.standard
         
-        let defaultLevel = userDefualts.integer(forKey: "Defualt Tip Level")
-        self.tipLevelSegmentControll.selectedSegmentIndex = defaultLevel
-        
-        self.billLabel.placeholder = "Whats the damage this time?"
+        let defaultLevel = userDefualts.integer(forKey: "Defualt_Tip_Level")
+        self.setCurrentTipLevel(defaultLevel)
     }
 
 
+/*       View action code   */
     @IBAction func touchOutsideTextField(_ sender: AnyObject) {
         print("Touch outside textfield")
         
         self.view.endEditing(true)
-        
     }
     
     @IBAction func editingDidEnd(_ sender: AnyObject) {
-        
-        print("Bill = %@", billLabel.text)
-        let costDouble = Double(self.billLabel.text!) ?? 0
-        let tip = costDouble * self.currentTipLevel()
-        self.changeTipToValue(tip)
-
-        let total = costDouble + tip
-        self.changeTotalToValue(total)
+        self.updateAllFields()
     }
+    
+/*       View manipulation code   */
     
     func changeTipToValue(_ value:Double) {
         print("Tip = \(value)")
@@ -73,5 +90,34 @@ class TipViewController: UIViewController {
     func currentTipLevel()->Double {
         return self.tipAmountArray[self.tipLevelSegmentControll.selectedSegmentIndex]
     }
+    
+    func setCurrentTipLevel(_ level:Int) {
+        self.tipLevelSegmentControll.selectedSegmentIndex = level
+    }
+    
+    
+/*       Math calculation code can be extracted to other class if needed   */
+    
+    func updateAllFields() {
+        let costDouble = Double(self.billLabel.text!) ?? 0
+        
+        let tip = self.caculateNewTipValue(costDouble)
+        self.changeTipToValue(tip)
+        
+        let total = self.calculateNewTotalValue(costDouble: costDouble, tipDouble: tip)
+        self.changeTotalToValue(total)
+    }
+    
+    func caculateNewTipValue(_ costDouble:Double) ->Double {
+        let tip = costDouble * self.currentTipLevel()
+        return tip
+    }
+    
+    func calculateNewTotalValue(costDouble:Double, tipDouble:Double) ->Double {
+        let total = costDouble + tipDouble
+        return total
+    }
+    
+
 }
 
