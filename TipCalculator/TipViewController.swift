ccc2563516af4df1f975f049d0c55e7b932eef17
Michael <realq86@gmail.com>
2016-09-21 18:50:38 -0700

    Remove unnecessary call to load default.

--- TipCalculator/TipViewController.swift
+++ TipCalculator/TipViewController.swift
@@ -23,9 +23,6 @@ class TipViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        self.loadDefualtSettings()
         print("viewDidLoad")
     }
     
